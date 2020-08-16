87b40ae4723f927a6d23c27cfea9b44d67209027
Harshita shree <harshita360@gmail.com>
2020-08-16 18:12:19 +0530

    Updates

---
+++ precache-manifest.790ba4fd8c23c3efa5f533b8601d4a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d6e6be900f5b206198b98155534424",
+    "url": "/LoginRegister-/index.html"
+  },
+  {
+    "revision": "8745641b22bfe61d6852",
+    "url": "/LoginRegister-/static/css/main.9ef602e7.chunk.css"
+  },
+  {
+    "revision": "2151b220283ca09aecea",
+    "url": "/LoginRegister-/static/js/2.7e1bb004.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/LoginRegister-/static/js/2.7e1bb004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8745641b22bfe61d6852",
+    "url": "/LoginRegister-/static/js/main.b9bb0fd6.chunk.js"
+  },
+  {
+    "revision": "e784f94354224e63bcec",
+    "url": "/LoginRegister-/static/js/runtime-main.b38ef292.js"
+  }
+]);
